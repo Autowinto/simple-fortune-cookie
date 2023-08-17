9d2caecbbfb845d1d227bc83dbe4d0ce45fd94c1
Aloxion <ansan21@student.sdu.dk>
2023-08-17 11:25:57 +0000

    Fixed username to autowinto, repo owner.

--- ci/push-docker.sh
+++ ci/push-docker.sh
@@ -2,3 +2,4 @@
 echo "$docker_password" | docker login ghcr.io --username "${docker_username,,}" --password-stdin
 docker push "ghcr.io/${docker_username,,}/fortune-cookie:1.0-${GIT_COMMIT::8}" 
 docker push "ghcr.io/${docker_username,,}/fortune-cookie:latest" &
+wait
